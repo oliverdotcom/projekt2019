603c15292f54ea973f53562d467cb9e76e754af9
Oliver Jützler <ojmac@Olivers-MacBook-Pro.local>
2019-11-11 15:51:29 +0100

    mobiletest5

--- script.js
+++ script.js
@@ -1,30 +1,21 @@
 onload = strt;
 
+/* Slideshow */
+
 function start() {
     var i = 1;
 
     function Move() {
-        i = (i % 5) + 1; // 4 is the Number of image in slider
+        i = (i % 5) + 1;
         document.getElementById('i' + i).checked = true;
     }
 
 
-    setInterval(Move, 4000); /* Adjust time before picture switch */
+    setInterval(Move, 4000);
 }
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
-}
+/* Navigation menu */
 
-
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
